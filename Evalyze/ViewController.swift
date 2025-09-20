58bdf4ed5bba447b01c0c01e2027db482003f170
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-09-20 15:14:53 +0300

    Initial commit

---
+++ Evalyze/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Evalyze
+//
+//  Created by Михаил Рычагов on 20.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+
+}
+
